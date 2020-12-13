5be8f066adeda28f7e4a28c5e9600ee0fbc6cbe6
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-13 09:36:08 +0100

    Created bin/test.py

---
+++ bin/test.py
@@ -0,0 +1,19 @@
+"""test
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-12-13"
+
+class test:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(test.test())
+	
+	def test():
+		"""test first funtion
+		"""
+		return "test" #for the test (see test_test.py)
+		
+if __name__ == "__main__":
+	test()
